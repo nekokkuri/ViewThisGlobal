e376816e11cc98b657607cfeb24b2be3030e4b89
Jet Rico <jetriconew@gmail.com>
2014-09-06 22:23:47 +0900

    Background.js Default Options Fix

--- js/background.js
+++ js/background.js
@@ -2,22 +2,26 @@
 ------------------------------------*/
 var default_options = {
 	region_cookies: 1,
-	profiles:[
-		
-	],
-	shirohime: {
-		screen_size: 2,
-		TL_inline: 1,
-		TL_image: 1,
-		ctouch: 1,
-		timer_screen: 1,
-		timer_sound: 1
-	}
+	profiles:[],
+	SH_screen_size: 2,
+	SH_TL_inline: 1,
+	SH_TL_image: 1,
+	SH_ctouch: 1,
+	SH_timer_screen: 1,
+	SH_timer_sound: 1
 };
 
+// Set to default if not yet set
 for(i in default_options){
 	if(typeof localStorage[i] == "undefined"){
 		localStorage[i] = default_options[i];
+	}
+}
+
+// Remove remnants of unused settings
+for(i in localStorage){
+	if(typeof default_options[i] == "undefined"){
+		delete localStorage[i];
 	}
 }
 
