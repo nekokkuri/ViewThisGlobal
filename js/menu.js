1cff9f8e74738cf4cf7cb096aac8a6ba87d296f2
Jet Rico <jetriconew@gmail.com>
2014-09-13 03:27:19 +0900

    Menu data and updated action scripts

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
 	
+	$("#profiles .profile").on('click', function(){
+		window.open("../players/"+$(this).data('game')+"/index.html#"+$(this).data('id'));
+	});
+	
+	$("#profiles .profile .refresh").on('click', function(e){
+		window.open("profile.html#"+$(this).parent().data('id'), "based_profile");
+		e.stopPropagation();
+	});
+	
 	$("#addnew").on('click', function(){
-		window.open("http://www.dmm.com/netgame");
-		window.close();
+		window.open("profile.html#0", "based_profile");
 	});
 	
 	$("#settings").on('click', function(){
 		window.open("options.html", "based_options");
+	});
+	
+	$("#cookies").on('click', function(){
+		chrome.runtime.sendMessage({
+			type: "BasedViewer",
+			action: "activeCookies"
+		}, function(response){});
 	});
 	
 	$("#shirohime").on('click', function(){
