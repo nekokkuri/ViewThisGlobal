cce59ca0f1d47c9c41d6564ca0ca94c1e956b718
Jet Rico <jetriconew@gmail.com>
2014-09-14 16:35:58 +0900

    Menu show Profile List

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,28 @@
+// Convert Game Name into Formal name
+function GetFormalGameName(GameName){
+	return {
+		"kancolle": "KANTAI COLLECTION",
+		"shirocolle": "SHIRO COLLECTION",
+		"olet": "OVER LEGEND ENDLESS TOWER",
+		"kanpani": "KANPANI",
+	}[GameName];
+}
+
 $(document).ready(function(){
+
+	// Show profile list
+	var CurrentProfiles = JSON.parse(localStorage["profiles"]);
+	var tmpProfileBox = {};
+	for(ProfileCtr in CurrentProfiles){
+		tmpProfileBox = $("#factory .profile").clone().appendTo("#profiles");
+		tmpProfileBox.data("id", CurrentProfiles[ProfileCtr].id);
+		tmpProfileBox.data("game", CurrentProfiles[ProfileCtr].game);
+		$(".icon img", tmpProfileBox).attr("src", "../img/profiles/"+CurrentProfiles[ProfileCtr].game+".png");
+		$(".game", tmpProfileBox).text(GetFormalGameName(CurrentProfiles[ProfileCtr].game));
+		$(".name", tmpProfileBox).text(CurrentProfiles[ProfileCtr].name);
+	}
 	
+	// Open a specific profile
 	$("#profiles .profile").on('click', function(){
 		window.open(
 			"../players/"+$(this).data('game')+"/index.html#"+$(this).data('id'),
@@ -7,19 +30,23 @@ $(document).ready(function(){
 		);
 	});
 	
+	// Refresh specific Profile
 	$("#profiles .profile .refresh").on('click', function(e){
 		window.open("profile.html#"+$(this).parent().data('id'), "based_profile");
 		e.stopPropagation();
 	});
 	
+	// Add a new Profile
 	$("#addnew").on('click', function(){
 		window.open("profile.html#0", "based_profile");
 	});
 	
+	// Configuration
 	$("#settings").on('click', function(){
 		window.open("options.html", "based_options");
 	});
 	
+	// Activate Region Cookies
 	$("#cookies").on('click', function(){
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {
@@ -31,10 +58,12 @@ $(document).ready(function(){
 		});
 	});
 	
+	// Play ShiroHime Quest
 	$("#shirohime").on('click', function(){
 		window.open("../players/shirohime/index.html", "based_play_shirohime");
 	});
 	
+	// Re-check menu sizing to prevent scrollbars
 	setTimeout(function(){
 		$("html,body").css("height", $("#wrapper").height()+"px");
 	},100);
