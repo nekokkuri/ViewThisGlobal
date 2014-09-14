388d7cbd81d9e9668a012c4a43cf0f429fe67b09
Jet Rico <jetriconew@gmail.com>
2014-09-15 02:17:58 +0900

    [OLET] Basic Player

---
+++ players/olet/script.js
@@ -0,0 +1,26 @@
+// Get Profile ID from hash tag of URL
+var ProfileID = window.location.hash.substring(1);
+var CurrentProfiles;
+
+$(document).ready(function(){
+
+	// Get all existing profiles
+	CurrentProfiles = JSON.parse(localStorage["profiles"]);
+	
+	// Find requested profile and execute
+	for(ProfileCtr in CurrentProfiles){
+		if(CurrentProfiles[ProfileCtr].id == ProfileID){
+			document.title = "OLET: "+CurrentProfiles[ProfileCtr].name;
+			$("#game_swf").attr("src", CurrentProfiles[ProfileCtr].link);
+		}
+	}
+	
+});
+
+$(window).on('resize', function(){
+	if($(document).height() > 500){
+		$("#wrapper").css("margin", "10px auto 0px");
+	}else{
+		$("#wrapper").css("margin", "0px auto");
+	}
+});
