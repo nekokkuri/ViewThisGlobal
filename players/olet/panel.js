076d45127b7b357beb940eea4e5797d14eef1831
Jet Rico <jetriconew@gmail.com>
2014-09-17 14:56:55 +0900

    [OLET] In-Player Logging

--- players/olet/panel.js
+++ players/olet/panel.js
@@ -31,25 +31,31 @@ function getApiCallName(queryString){
 function handleContent(ApiCall, response){
 	switch(ApiCall){
 		case "login":
+			$("#logs").append("[OLET] Initial login<br />");
 			console.info("[OLET] Initial login");
 			console.log(response);
 			break;
 		case "constant":
+			$("#logs").append("[OLET] Game constants received<br />");
 			console.info("[OLET] Game constants received");
 			console.log(response);
 			break;
 		case "homeinit":
+			$("#logs").append("[OLET] Home page data received<br />");
 			console.info("[OLET] Home page data received");
 			console.log(response);
 			break;
 		case "reqinit":
+			$("#logs").append("[OLET] List of Quests received<br />");
 			console.info("[OLET] List of Quests received");
 			console.log(response);
 			break;
 		case "iteminit":
+			$("#logs").append("[OLET] List of Player Items received<br />");
 			console.info("[OLET] List of Player Items received");
 			console.log(response);
 		case "batheinit":
+			$("#logs").append("[OLET] Onsen<br />");
 			console.info("[OLET] Onsen");
 			console.log(response);
 			break;
