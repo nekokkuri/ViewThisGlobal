7123ec0962a494beb16e06bd9189e8fcdb83b8cf
Jet Rico <jetriconew@gmail.com>
2014-09-27 17:07:57 +0900

    [Kanpani] Rename player assets

---
+++ players/kanpani/index.js
@@ -0,0 +1,33 @@
+// Get Profile ID from hash tag of URL
+var ProfileID = window.location.hash.substring(1);
+var CurrentProfiles;
+
+$(document).ready(function(){
+
+	// Get all existing profiles
+	CurrentProfiles = JSON.parse(localStorage["profiles"]);
+	
+	// Find requested profile and execute
+	for(ProfileCtr in CurrentProfiles){
+		if(CurrentProfiles[ProfileCtr].id == ProfileID){
+			document.title = "Kanpani: "+CurrentProfiles[ProfileCtr].name;
+			$("#game_swf").attr("src", CurrentProfiles[ProfileCtr].link);
+		}
+	}
+	
+	var obj = {
+		a:"b",
+		c:"d"
+	};
+	var amfs = AMF.stringify(obj);
+	console.log(amfs);
+	console.log(AMF.parse(amfs));
+});
+
+$(window).on('resize', function(){
+	if($(document).height() > 500){
+		$("#wrapper").css("margin", "10px auto 0px");
+	}else{
+		$("#wrapper").css("margin", "0px auto");
+	}
+});
